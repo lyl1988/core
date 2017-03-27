71b3edadebb663a5f91454195b5712efc0839aaf
Sujith H <sharidasan@owncloud.com>
2017-03-27 09:23:30 +0530

    This change helps user to switch between master and user specific keys.
    Signed-off-by: Sujith H <sharidasan@owncloud.com>

--- settings/js/admin.js
+++ settings/js/admin.js
@@ -41,6 +41,85 @@ $(document).ready(function(){
 		$('#encryptionAPI div#EncryptionWarning').toggleClass('hidden');
 	});
 
+	// This function will help to arrange the display of the encryption page
+	// in the settings.
+	function encryptionDisplay (encryptionType, state=undefined) {
+		if (encryptionType === "nokey") {
+			//If user selects "Please sect an encryption option"
+			$('#confirm-encryption-warning').addClass("hidden");
+			$('#encryptionAPI').addClass("hidden");
+			$('#ocDefaultEncryptionModule').addClass("hidden");
+			$('#encryptHomeStorage').removeAttr("checked",false);
+		} else if (encryptionType === "masterkey") {
+			//If user selects "Master Key" from the drop down
+			if(state === "static") {
+				$("#keyTypeId").prop("disabled",true);
+				$('#confirm-encryption-warning').addClass("hidden");
+			} else {
+				$('#confirm-encryption-warning').removeClass("hidden");
+			}
+			$('#encryptionAPI').addClass("hidden");
+			$('#ocDefaultEncryptionModule').addClass("hidden");
+			$('#encryptHomeStorage').removeAttr("checked", false);
+			$('#enableEncryption').removeAttr("checked", false);
+			$('#OC_DEFAULT_MODULE').removeAttr("checked", false);
+			$('#enableEncryption').removeAttr("disabled", false);
+		} else {
+			//If user selects "User-specific key from the drop down"
+			$('#encryptHomeStorage').attr("checked","checked");
+			$('#confirm-encryption-warning').addClass("hidden");
+			$('#encryptionAPI').removeClass("hidden");
+			$('#ocDefaultEncryptionModule').removeClass("hidden");
+			if(state === "static") {
+				$("#keyTypeId").prop("disabled",true);
+				$("#encryptionSetRecoveryKey").addClass("hidden");
+				OC.AppConfig.getValue("encryption", "encryptHomeStorage", function ($value) {
+					if($value === '0') {
+						$("#encryptHomeStorage").removeAttr("checked","checked");
+						$("#encryptHomeStorage").attr("value","0");
+					}
+				});
+			}
+		}
+	}
+
+	//This is the condition where the page has to be before/after selection.
+	encryptionDisplay($("#keyTypeId :selected").val(), "static");
+
+	if ($('#enableEncryption').prop("checked") === true) {
+		if($("#keyTypeId :selected").val() !== "masterkey") {
+			$('#keyTypeId option[value="customkey"]').attr("selected", "selected");
+			$('#confirm-encryption-warning').removeClass("hidden");
+			encryptionDisplay("customkey", "static");
+		}
+	}
+
+	//This is where the drop down onChange happens.
+	$("#keyTypeId").change(function (element) {
+		encryptionDisplay($("#keyTypeId :selected").val());
+	});
+
+	//After clicking final confirmation for master-key action taken.
+	$('#reconfirm-encryption-type').click(function (event) {
+		if ($('#keyTypeId option:selected').val() == 'masterkey') {
+			OC.AppConfig.setValue('core', 'encryption_enabled', 'yes');
+			OC.AppConfig.setValue("encryption", "encryptHomeStorage", '0');
+			OC.AppConfig.setValue('encryption', 'useMasterKey', '1');
+			$(document).ajaxStop(function () {
+				location.reload();
+			});
+		}
+	});
+
+	//After selecting user specific key, action taken.
+	$("#reallyEnableEncryption").click(function (event) {
+		if ($("#encryptHomeStorage").prop("checked") === true) {
+			OC.AppConfig.setValue("encryption", "encryptHomeStorage", '1');
+		} else {
+			OC.AppConfig.setValue("encryption", "encryptHomeStorage", '0');
+		}
+	});
+
 	$('#reallyEnableEncryption').click(function() {
 		$('#encryptionAPI div#EncryptionWarning').toggleClass('hidden');
 		$('#encryptionAPI div#EncryptionSettingsArea').toggleClass('hidden');
